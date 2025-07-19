5ede23226a71130d621d63223cbfe6163a11705e
IMSHIVAMRAJ <sraj2004bxr@gmail.com>
2025-07-19 18:21:24 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -2,32 +2,27 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
- 
-// Load env variables
+
 dotenv.config();
- 
-// Connect to MongoDB
 connectDB();
- 
-// Init app
+
 const app = express();
 
-// Middlewares
-app.use(cors());
+const corsOptions = {
+  origin: ['http://localhost:5173', 'https://hack-verse-sr.vercel.app'], // ✔️ allowed frontend domain
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/user', require('./routes/userRoutes'));
 app.use('/api/requirements', require('./routes/requirementRoutes'));
 
-// Base route
-app.get('/', (req, res) => { 
+app.get('/', (req, res) => {
   res.send('API is Running...');
 });
 
-// Server start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
-
-
