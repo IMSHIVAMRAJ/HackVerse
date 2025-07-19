d00fc618c95346ad28b25d923bb726fe9552d7f3
IMSHIVAMRAJ <sraj2004bxr@gmail.com>
2025-07-19 19:02:58 +0530

    1

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,17 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://hack-verse-sr.vercel.app'], // ✔️ allowed frontend domain
+  origin: function (origin, callback) {
+    const allowedOrigins = ['http://localhost:5173', 'https://hack-verse-sr.vercel.app'];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 };
+
 
 app.use(cors(corsOptions));
 app.use(express.json());
